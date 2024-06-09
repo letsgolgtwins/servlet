11cabcf71014d6eef852157f549dcdba976d38d8
LeeMinHee <98minh2@gmail.com>
2024-06-09 15:21:14 +0900

    240609

--- src/main/java/com/test/lesson03/DeleteQuiz02.java
+++ src/main/java/com/test/lesson03/DeleteQuiz02.java
@@ -1,5 +1,8 @@
 package com.test.lesson03;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +15,7 @@ public class DeleteQuiz02 extends HttpServlet {
 	
 	@Override
 	public void doGet(HttpServletRequest request
-			,HttpServletResponse response) {
+			,HttpServletResponse response) throws IOException {
 		
 		// rsc 생략
 		
@@ -25,9 +28,18 @@ public class DeleteQuiz02 extends HttpServlet {
 		
 		// deleteQuery
 		// 여기서부터 하면 된다.
+		String deleteQuery = "delete from `bookmark` where `id`=" + id;
+		try {
+			ms.update(deleteQuery);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 		// db연결 해제
 		ms.disconnect();
+		
+		// 302 리닥& 예외처리
+		response.sendRedirect("/lesson03/quiz02.jsp");
 	}
 }
 
