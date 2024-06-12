c55b2d2cd0b11a91c0fcbe898b98e54c29d5ce9a
LeeMinHee <98minh2@gmail.com>
2024-06-12 15:56:19 +0900

    quiz01 3번 4번

--- src/main/java/JavaQuizRetry/lesson07/Quiz01.java
+++ src/main/java/JavaQuizRetry/lesson07/Quiz01.java
@@ -3,13 +3,9 @@ package JavaQuizRetry.lesson07;
 public class Quiz01 {
 
 	public static void main(String[] args) {
-		int[][] scores = { 
-				{ 89, 93, 91, 93, 92, 78, 90, 90, 93, 90 }, 
-				{ 91, 82, 72, 98, 92, 87, 77, 87, 74, 88 },
-				{ 98, 93, 94, 91, 97, 94, 91, 96, 98, 90 }, 
-				{ 65, 63, 57, 87, 88, 92, 78, 85, 100, 68 },
-				{ 45, 50, 48, 63, 67, 58, 40, 66, 47, 64 } 
-		};
+		int[][] scores = { { 89, 93, 91, 93, 92, 78, 90, 90, 93, 90 }, { 91, 82, 72, 98, 92, 87, 77, 87, 74, 88 },
+				{ 98, 93, 94, 91, 97, 94, 91, 96, 98, 90 }, { 65, 63, 57, 87, 88, 92, 78, 85, 100, 68 },
+				{ 45, 50, 48, 63, 67, 58, 40, 66, 47, 64 } };
 
 		// quiz01_1. 학생별 평균 구하기
 		for (int i = 0; i < scores.length; i++) {
@@ -31,10 +27,12 @@ public class Quiz01 {
 			}
 			System.out.println((i + 1) + "번째 학생의 최고점은 " + max);
 		}
-		
+
 		// quiz01_3. 평균 최고점 구하기 - 평균이 가장 높은 학생의 평균점수와 몇 번째 학생인지 출력하세요.
 		// 이 문제부터 이따 다시 풀기
 		System.out.println();
+		double maxAverage = 0;
+		int maxAverageIndex = 0;
 		for (int i = 0; i < scores.length; i++) {
 			double sum3 = 0;
 			for (int j = 0; j < scores[i].length; j++) {
@@ -42,7 +40,27 @@ public class Quiz01 {
 			}
 			double average = sum3 / scores[i].length;
 //			System.out.println(average); 평균
+			if (maxAverage < average) {
+				maxAverage = average;
+				maxAverageIndex = i;
+			}
 		}
+		System.out.println("평균이 가장 높은 학생의 평균 점수는 : " + maxAverage);
+		System.out.println("그 학생은 : " + (maxAverageIndex + 1) + "번째 학생");
+
+		// quiz01_4. 특정 과목 최고점 - 4번째 과목의 최고 성적의 학생이 몇 번째 학생인지와 점수를 출력하세요.
+		System.out.println();
+		int maxScore = 0;
+		int maxScoreIndex = 0;
+		for (int i = 0; i < scores.length; i++) {
+			// int maxScore = scores[i][3];
+			if (maxScore < scores[i][3]) {
+				maxScore = scores[i][3];
+				maxAverageIndex = i;
+			}
+		}
+		System.out.println("4번째 과목의 최고 성적의 학생은 " + (maxAverageIndex + 1) + "번째 학생이고");
+		System.out.println("그 점수는 " + maxScore + "점.");
 
 	}
 
