755ccb86ea3fa7cf41d5e5fb2c31a29253df10af
LeeMinHee <98minh2@gmail.com>
2024-09-03 12:45:11 +0900

    240903

--- src/main/java/JavaQuizRetry/lesson08/Quiz04.java
+++ src/main/java/JavaQuizRetry/lesson08/Quiz04.java
@@ -22,36 +22,36 @@ public class Quiz04 {
 		// 아래와 같이 4개의 문제를 출제하고, 문자열로 차례차례 입력을 받는다.
 		// 100점 만점 기준으로 몇점인지 출력한다.
 		// <출력예시> 점수는 75점 입니다.
-//		int score = 0;
-//		System.out.print("1. 승리를 영어로 입력하세요 : ");
-//		String victory = scan.next();
-//		System.out.print("2. 사랑을 영어로 입력하세요 : ");
-//		String love = scan.next();
-//		System.out.print("3. 컴퓨터를 영어로 입력하세요 : ");
-//		String computer = scan.next();
-//		System.out.print("4. 노트북을 영어로 입력하세요 : ");
-//		String notebook = scan.next();
-//		if (victory.equals("victory")) {
-//			score++;
-//		} else {
-//
-//		}
-//		if (love.equals("love")) {
-//			score++;
-//		} else {
-//
-//		}
-//		if (computer.equals("computer")) {
-//			score++;
-//		} else {
-//
-//		}
-//		if (notebook.equals("notebook")) {
-//			score++;
-//		} else {
-//
-//		}
-//		System.out.println("점수는 " + (score * 25) + "점 입니다.");
+		int score = 0;
+		System.out.print("1. 승리를 영어로 입력하세요 : ");
+		String victory = scan.next();
+		System.out.print("2. 사랑을 영어로 입력하세요 : ");
+		String love = scan.next();
+		System.out.print("3. 컴퓨터를 영어로 입력하세요 : ");
+		String computer = scan.next();
+		System.out.print("4. 노트북을 영어로 입력하세요 : ");
+		String notebook = scan.next();
+		if (victory.equals("victory")) {
+			score++;
+		} else {
+
+		}
+		if (love.equals("love")) {
+			score++;
+		} else {
+
+		}
+		if (computer.equals("computer")) {
+			score++;
+		} else {
+
+		}
+		if (notebook.equals("notebook")) {
+			score++;
+		} else {
+
+		}
+		System.out.println("점수는 " + (score * 25) + "점 입니다.");
 
 		// 3. 동명이인 수 구하기
 		// 동명이인 종류: 송강호, 이민정, 이정재 => 3
