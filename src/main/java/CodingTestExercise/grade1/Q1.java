8755d1206785bffe4c8e97c4d95e3e899aadd4cb
LeeMinHee <98minh2@gmail.com>
2024-11-06 16:11:59 +0900

    241106 1st

---
+++ src/main/java/CodingTestExercise/grade1/Q1.java
@@ -0,0 +1,48 @@
+package CodingTestExercise.grade1;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		// 01
+		System.out.println("Hello World!");
+	
+		// 02
+		System.out.print(""); // 줄바꿈
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.println(a + b);
+		
+		// 03
+		System.out.print(""); // 줄바꿈
+		a = scan.nextInt();
+		b = scan.nextInt();
+		System.out.println(a - b);
+		
+		// 04
+		System.out.print(""); // 줄바꿈
+		a = scan.nextInt();
+		b = scan.nextInt();
+		System.out.println(a * b);
+		
+		// 05
+		System.out.print(""); // 줄바꿈
+		a = scan.nextInt();
+		b = scan.nextInt();
+		System.out.println(a / b);
+		
+		// 06
+		System.out.print(""); // 줄바꿈
+		a = scan.nextInt();
+		b = scan.nextInt();
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		
+		
+	}
+
+}
